9813aa7d8e16f1941e144fe2bfaf3c06c5a69dde
Chris Tong <catong51@gmail.com>
2026-06-22 09:29:23 -0400

    Fix nvim autotag Telescope crash

--- nvim/lua/ctong/plugins/autotags.lua
+++ nvim/lua/ctong/plugins/autotags.lua
@@ -1,6 +1,23 @@
 return {
   "windwp/nvim-ts-autotag",
-  config = function()
-    require('nvim-ts-autotag').setup()
-  end
+  ft = {
+    "html",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "tsx",
+    "jsx",
+    "xml",
+    "markdown",
+  },
+  opts = {
+    opts = {
+      -- Work around nvim-ts-autotag trying to rename tags from buffers
+      -- without an active treesitter parser (for example Telescope prompt).
+      enable_rename = false,
+      enable_close = true,
+      enable_close_on_slash = false,
+    },
+  },
 }
